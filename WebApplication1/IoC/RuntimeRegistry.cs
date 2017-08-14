f2306c9ec445216ae881e6431c5fb37bab349d56
EliiseS <selingeliise@gmail.com>
2017-08-14 11:04:26 +0200

    Add project files

---
+++ WebApplication1/IoC/RuntimeRegistry.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using StructureMap;
+
+namespace WebApplication1.IoC
+{
+    public class RuntimeRegistry : Registry
+    {
+        public RuntimeRegistry()
+        {
+            Scan(x =>
+            {
+                x.Assembly(GetType().Assembly);
+                x.ConnectImplementationsToTypesClosing(typeof(IRequestHandler<>)); // Handlers with no response
+                x.ConnectImplementationsToTypesClosing(typeof(IRequestHandler<,>)); // Handlers with a response
+                x.ConnectImplementationsToTypesClosing(typeof(IAsyncRequestHandler<>)); // Async handlers with no response
+                x.ConnectImplementationsToTypesClosing(typeof(IAsyncRequestHandler<,>)); // Async Handlers with a response
+                x.ConnectImplementationsToTypesClosing(typeof(INotificationHandler<>));
+                x.ConnectImplementationsToTypesClosing(typeof(IAsyncNotificationHandler<>));
+                //x.AddAllTypesOf(typeof(IRequestHandler<,>));
+                //x.AddAllTypesOf(typeof(IAsyncRequestHandler<,>));
+                //x.AddAllTypesOf(typeof(INotificationHandler<>));
+                //x.AddAllTypesOf(typeof(IAsyncNotificationHandler<>));
+                x.AssembliesAndExecutablesFromApplicationBaseDirectory();
+                x.TheCallingAssembly();
+                x.LookForRegistries();
+                x.WithDefaultConventions();
+            });
+
+            //Singleton
+            //For(typeof(IRepository)).Singleton().Use(typeof(Repository));
+            //For<IExceptionHandler>().Singleton().Use<ExceptionHandler>();
+            //For<ILoggerFactory>().Singleton().Use<LoggerFactory>();
+            //For(typeof(ILogger<>)).Singleton().Add(typeof(Logger<>));
+            //For<ILogManager>().Singleton().Use<LogManager>();
+
+            //Setting up Mediatr
+            For<SingleInstanceFactory>().Use<SingleInstanceFactory>(ctx => t => ctx.GetInstance(t));
+            For<MultiInstanceFactory>().Use<MultiInstanceFactory>(ctx => t => ctx.GetAllInstances(t));
+            For<IMediator>().Use<Mediator>();
+        }
+    }
+}
---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using NLog.Web;
+using StructureMap;
+using System;
+using System.Diagnostics;
+using WebApplication1.IoC;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+
+            services.AddSession();
+
+            //services.AddDbContext<MIWBillingContext>();
+
+            return ConfigureIoC(services);
+        }
+
+        public IServiceProvider ConfigureIoC(IServiceCollection services)
+        {
+            var container = new Container(new RuntimeRegistry());
+            Trace.TraceInformation(container.WhatDoIHave());
+            container.Configure(config => config.Populate(services));
+
+            return container.GetInstance<IServiceProvider>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseSession();
+            app.UseStaticFiles();
+
+            loggerFactory.AddNLog();
+            env.ConfigureNLog("nlog.config");
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
