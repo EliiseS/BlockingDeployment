20dac8c4e7a3c15d74c704232bcf049da0cfa881
EliiseS <selingeliise@gmail.com>
2017-08-15 11:38:48 +0200

    Remove unused items

--- WebHost/IoC/RuntimeRegistry.cs
+++ WebHost/IoC/RuntimeRegistry.cs
@@ -11,24 +11,19 @@ namespace WebApplication1.IoC
             {
                 x.Assembly(GetType().Assembly);
                 //TODO: Below section of commented code causes the issue with deploying
-                //x.ConnectImplementationsToTypesClosing(typeof(IRequestHandler<>)); // Handlers with no response
-                //x.ConnectImplementationsToTypesClosing(typeof(IRequestHandler<,>)); // Handlers with a response
-                //x.ConnectImplementationsToTypesClosing(typeof(IAsyncRequestHandler<>)); // Async handlers with no response
-                //x.ConnectImplementationsToTypesClosing(typeof(IAsyncRequestHandler<,>)); // Async Handlers with a response
-                //x.ConnectImplementationsToTypesClosing(typeof(INotificationHandler<>));
-                //x.ConnectImplementationsToTypesClosing(typeof(IAsyncNotificationHandler<>));
+                x.ConnectImplementationsToTypesClosing(typeof(IRequestHandler<>)); // Handlers with no response
+                x.ConnectImplementationsToTypesClosing(typeof(IRequestHandler<,>)); // Handlers with a response
+                x.ConnectImplementationsToTypesClosing(typeof(IAsyncRequestHandler<>)); // Async handlers with no response
+                x.ConnectImplementationsToTypesClosing(typeof(IAsyncRequestHandler<,>)); // Async Handlers with a response
+                x.ConnectImplementationsToTypesClosing(typeof(INotificationHandler<>));
+                x.ConnectImplementationsToTypesClosing(typeof(IAsyncNotificationHandler<>));
+                // End of the section causing the issue
+
                 x.AssembliesAndExecutablesFromApplicationBaseDirectory();
                 x.TheCallingAssembly();
                 x.LookForRegistries();
                 x.WithDefaultConventions();
             });
-
-            //Singleton
-            //For(typeof(IRepository)).Singleton().Use(typeof(Repository));
-            //For<IExceptionHandler>().Singleton().Use<ExceptionHandler>();
-            //For<ILoggerFactory>().Singleton().Use<LoggerFactory>();
-            //For(typeof(ILogger<>)).Singleton().Add(typeof(Logger<>));
-            //For<ILogManager>().Singleton().Use<LogManager>();
 
             //Setting up MediatR
             For<SingleInstanceFactory>().Use<SingleInstanceFactory>(ctx => t => ctx.GetInstance(t));
